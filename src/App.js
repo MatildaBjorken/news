e8718161236efee0c8793e7118198ad6f3e89b63
MatildaBjorken <matilda.bjork@hyperisland.se>
2021-06-29 11:22:16 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -132,7 +132,7 @@ const App = () => {
             <Switch>
               <Route
                 exact
-                path="/"
+                path="/news"
                 render={() => (
                   <>
                     <StyleRoot>
